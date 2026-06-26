b13d332feac0138670a646f89280fe36ac3851ae
Guide <guiderock55@gmail.com>
2026-06-26 13:55:00 +0700

    Register @fastify/multipart plugin (v8, compatible with Fastify 4)

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import Fastify from "fastify";
 import { userRoutes } from "./routes/user.routes";
+import multipart from "@fastify/multipart";
 
 const app = Fastify({ logger: true });
 const PORT = Number(process.env.PORT) || 3002;
 
 app.register(userRoutes);
+app.register(multipart);
 
 app.listen({ port: PORT }, (err) => {
   if (err) {
